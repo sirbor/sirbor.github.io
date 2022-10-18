20cf9c6a5d2d55c6b7ed38a18647199e9fe6a0c3
Dominic Bor <dominicbor44@gmail.com>
2022-10-18 23:02:27 +0300

    updated commit

--- src/components/About.js
+++ src/components/About.js
@@ -66,7 +66,7 @@ const About = () => {
       >
         Resume
       </a>
-      <div >
+      <div className="mb-10" >
         <a href="https://www.buymecoffee.com/sirbor"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" width="200" />
         </a>
       </div>
